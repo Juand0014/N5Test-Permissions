c8ca42d2f4cfb0acfbd8c45e782e54f3e3ce0f74
JMatos0014 <matos@transformait.io>
2024-09-25 12:13:03 -0400

    feat(GetPermissionTypeByIdQuery): add get by id

---
+++ PermissionsAPI/CQRS/Queries/GetPermissionTypeByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using PermissionsAPI.Data;
+using PermissionsAPI.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PermissionsAPI.CQRS.Queries
+{
+    public class GetPermissionTypeByIdQueryHandler : IRequestHandler<GetPermissionsTypeByIdQuery, PermissionType>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public GetPermissionTypeByIdQueryHandler(IUnitOfWork _unitOfWork, IMapper _mapper)
+        {
+            this._unitOfWork = _unitOfWork;
+            this._mapper = _mapper;
+        }
+        public async Task<PermissionType> Handle(GetPermissionsTypeByIdQuery request, CancellationToken cancellationToken)
+        {
+            var permissionType = await _unitOfWork.PermissionTypes.GetById(request.Id);
+            return _mapper.Map<PermissionType>(permissionType);
+        }
+    }
+}
